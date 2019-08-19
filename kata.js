d00b7cc8deae54758894136cded9503ba197925c
shehanpeiris <shehanpeiris@gmail.com>
2019-08-18 20:06:47 -0500

    Create Phone Number

--- kata.js
+++ kata.js
@@ -344,6 +344,47 @@ function driver(data) {
 // #########################################################################################################
 
 // ========================================
+//          Create Phone Number
+// ========================================
+
+// CHALLENGE
+// ----------------------------------------
+// Write a function that accepts an array of 10 integers (between 0 and 9), and that returns a string of those
+// numbers in the form of a phone number.
+// Example:
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// ----------------------------------------
+
+// MY SOLUTION
+// ----------------------------------------
+// ----------------------------------------
+
+// SOLUTIONS FROM THE CROWD
+// ----------------------------------------
+function createPhoneNumber(numbers){
+  numbers = numbers.join('');
+  return '(' + numbers.substring(0, 3) + ') ' 
+      + numbers.substring(3, 6) 
+      + '-' 
+      + numbers.substring(6);
+};
+
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+};
+// ----------------------------------------
+
+// #########################################################################################################
+// #########################################################################################################
+
+// ========================================
 //             [INSERT TITLE]
 // ========================================
 
